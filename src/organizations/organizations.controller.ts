9754040944524085617699b616300bbc4a28ad63
tayssa <tayssavictoria2004@gmail.com>
2026-01-13 20:56:20 -0300

    🚑️ Keep the organization's post protected with Nest Guard
    Keep the organization's post protected with Nest Guard

--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('organizations')
 export class OrganizationsController {
   constructor(private readonly organizationsService: OrganizationsService) {}
+
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() dto: CreateOrganizationDto) {
     return this.organizationsService.create(dto);
--- src/organizations/organizations.module.ts
+++ src/organizations/organizations.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [OrganizationsService],
   controllers: [OrganizationsController],
 })
